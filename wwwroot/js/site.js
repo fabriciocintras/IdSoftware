d0183cd5d705538ee0ac219e7eb088432f846bcd
fabricio cintra <fabricioalvescintra@gmail.com>
2020-11-28 00:47:42 -0300

    Projeto IdSoftware com Bootstrap4

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
 
-// Write your JavaScript code.
+                          
+  $('#formContato').on('submit', function(e){
+    $('#modalConfirma').modal('show');
+    e.preventDefault();
+  });
